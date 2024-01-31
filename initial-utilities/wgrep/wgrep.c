0d7fa22f7c7ad35d5bc9eeb51ea965adc15a8de9
ferndz <nafernandez@mi.unc.edu.ar>
2024-01-31 12:30:22 -0300

    All tests passed

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -3,21 +3,9 @@
 #include <stdbool.h>
 #include <string.h>
 #include <errno.h>
-#define N 1024
+#define N 10000
 
-int main(int argc, char* argv[]){
-    if (argc != 3){
-        printf("wgrep: searchterm [file ...]\n");
-        exit(1);
-    }
-    char* word = argv[1];
-    FILE* fp = fopen(argv[2], "r");
-    if(fp == NULL){
-        printf("wgrep: cannot open file\n");
-        exit(1);
-    }
-    size_t tam = N;
-    char* buffer = malloc(N * sizeof(char));
+void search_from_arg(char* buffer, size_t tam, FILE* fp, char* word){
     while(getline(&buffer, &tam, fp) != -1){
        bool founded = false;
        unsigned int j = 0;
@@ -37,8 +25,37 @@ int main(int argc, char* argv[]){
         if(founded){
             printf("%s", buffer);
          }
+        tam = N;
     }
-    free(buffer);   
-    fclose(fp);
+}
+
+/*void search_from_stdin(char* buffer, unsigned int tam, FILE* fp, char* word)
+{
+  
+}*/
+
+
+int main(int argc, char* argv[]){
+    if (argc < 2){
+        printf("wgrep: searchterm [file ...]\n");
+        exit(1);
+    }
+    size_t tam = N;
+    char* buffer = malloc(N * sizeof(char));
+    char* word = argv[1];
+    FILE* fp = NULL;
+    if (argc == 3){
+        fp = fopen(argv[2], "r");
+        if(fp == NULL){
+            printf("wgrep: cannot open file\n");
+            exit(1);
+        }
+        search_from_arg(buffer, tam, fp, word);
+        fclose(fp);
+    }
+    else{
+        search_from_arg(buffer, tam, stdin , word);
+    }
+    free(buffer);
     return 0;
 }
