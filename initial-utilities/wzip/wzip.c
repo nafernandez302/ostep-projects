7e2ca2d1994d944d20c18ff71e39f273d4a64326
ferndz <nafernandez@mi.unc.edu.ar>
2024-02-01 20:01:17 -0300

    source file is readed and saved in the buffer

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -3,21 +3,25 @@
 
 #define BYTES_SIZE 4
 #define N 1024
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
 int main(int argc, char* argv[]){
 
     // happy path: take one file, and returns file compressed
-    // my example: ./wzip dst src
+    // my example: ./wzip dst src -->return src compressed into dst
 
     FILE* src = NULL;
     FILE* dst = NULL;
 
     src = fopen(argv[1], "r");
-    dst = fopen(argv[2], "rw");
+    dst = fopen(argv[2], "w+");
 
     size_t bytes_long = BYTES_SIZE;
     size_t max_size = N;
-    size_t readed = fwrite(src, max_size, bytes_long, dst);
+    char* buffer = malloc(N * sizeof(char));
 
+    size_t readed = fread(buffer, sizeof(char), ARRAY_SIZE(buffer), src);
+
+    printf("readed: %lu\n", readed);
     return 0;
 }
